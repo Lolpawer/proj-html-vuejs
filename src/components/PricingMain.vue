2a273eadfa289f83d20a7fa8d31789a4797df6d8
Lolpawer <dadu.petrosino@gmail.com>
2022-02-09 20:18:06 +0100

    added pricing

--- src/components/PricingMain.vue
+++ src/components/PricingMain.vue
@@ -4,18 +4,103 @@
             <div class="lightseagreen-text bold">MONTHLY PLANS</div>
             <div class="darkslategray-text section-title-font title">Pricing Table</div>
             <div class="lightslategray-text subtext">We can help you deliver and execute your future, wherever you compete, using the latest technologies, from strategy development to implementation.</div>
-            <div class="flex space-between">
+            <div class="flex space-between pricing">
 
                 <div class="pricing-table">
-                    <i class="fas fa-rocket teal-text"></i>
+                    <i class="fas fa-rocket teal-text icon"></i>
+                    <p class="darkslategray-text bold pricing-title icon">Standard</p>
+                    <p class="teal-text price-text icon">
+                        $
+                        <span class="price bold">490</span>
+                        / mo
+                    </p>
+                    <ul>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">National Coverage</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Unlimited Motoboy</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Address Collection</p>
+                            <i class="fas fa-times teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Mobile App Tracking</p>
+                            <i class="fas fa-times teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Cargo Insurance</p>
+                            <i class="fas fa-times teal-text"></i>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="pricing-table pricing-middle">
+                    <div class="most-popular">Most Popular</div>
+                    <i class="fas fa-briefcase teal-text icon"></i>
+                    <p class="darkslategray-text bold pricing-title icon">Professional</p>
+                    <p class="teal-text price-text icon">
+                        $
+                        <span class="price bold">890</span>
+                        / mo
+                    </p>
+                    <ul>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">National Coverage</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Unlimited Motoboy</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Address Collection</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Mobile App Tracking</p>
+                            <i class="fas fa-times teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Cargo Insurance</p>
+                            <i class="fas fa-times teal-text"></i>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="pricing-table">
-                    <i class="fas fa-briefcase teal-text"></i>
-                </div>
-
-                <div class="pricing-table">
-                    <i class="fas fa-gem teal-text"></i>
+                    <i class="fas fa-gem teal-text icon"></i>
+                    <p class="darkslategray-text bold pricing-title icon">Ultimate</p>
+                    <p class="teal-text price-text icon">
+                        $
+                        <span class="price bold">1.390</span>
+                        / mo
+                    </p>
+                    <ul>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">National Coverage</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Unlimited Motoboy</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Address Collection</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Mobile App Tracking</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                        <li class="flex space-between">
+                            <p class="lightslategray-text pricing-text">Cargo Insurance</p>
+                            <i class="fas fa-check teal-text"></i>
+                        </li>
+                    </ul>
                 </div>
 
             </div>
@@ -46,9 +131,66 @@
                 padding: 0 160px;
             }
 
-            .pricing-table {
-                width: 380px;
-                background-color: #fff;
+            .pricing {
+                padding: 50px 0;
+
+                .pricing-table {
+                    width: 380px;
+                    height: 500px;
+                    background-color: #fff;
+                    padding: 40px 0;
+                    margin-top: 10px;
+                    border-radius: 10px;
+
+                    .icon {
+                        font-size: 45px;
+                    }
+
+                    .pricing-title {
+                        font-size: 30px;
+                        padding: 10px 0;
+                    }
+
+                    .price-text {
+                        font-size: 20px;
+
+                        .price {
+                            font-size: 50px;
+                        }
+                    }
+
+                    ul {
+                        padding: 0 50px;
+
+                        li {
+                            padding: 15px 0;
+                        }
+
+                        .pricing-text {
+                            font-size: 20px;
+                        }
+                    }
+                }
+
+                .pricing-middle {
+                    height: 540px;
+                    padding: 50px 0;
+                    margin-top: 0;
+                    position: relative;
+
+                    .most-popular {
+                        position: absolute;
+                        width: 80px;
+                        text-align: start;
+                        padding: 10px 10px;
+                        background-color: #00a6a6;
+                        color: #fff;
+                        border-top-right-radius: 25px;
+                        border-bottom-right-radius: 25px;
+                        top: 12px;
+                        font-size: 14px;
+                    }
+                }
             }
         }
     }
